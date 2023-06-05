0ffa02f59613a938e5643970cc120c45763fc4c0
Mihkel Väli <mihkel.vali@hotmail.com>
2023-06-05 18:51:00 +0300

    Add cors support

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const pool = require('./config');
 
 const app = express();
 app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
 
 app.get('/', (req, res) => {
     res.send('Simple API homepage');
